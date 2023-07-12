757d07d72e4388a5ff09c31fd4486cef78532b43
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-07-12 15:56:38 +0100

    api exposed class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
     @GetMapping
-    public List<String> hello(){
-        return List.of("Hello","World");
+    public List<Student> hello() {
+        return List.of(
+                new Student(1L, "walid", LocalDate.of(2001, 1, 25), 22, "walid@gmail.com"),
+                new Student(2L, "ranim", LocalDate.of(2008, 1, 2), 15, "ranime@live.com")
+        );
     }
 
 }
